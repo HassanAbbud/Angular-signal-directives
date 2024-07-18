0e1080b6f95d5e65820b11691f157d397d0dd5ed
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-18 17:10:46 -0600

    Reactive form setup

--- src/app/products/pages/product-page/product-page.component.ts
+++ src/app/products/pages/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'products-product-page',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './product-page.component.css'
 })
 export class ProductPageComponent {
+  public myForm: FormGroup;
+  //private fb = inject(FormBuilder) -> Another way of injecting
+  public color: string;
+
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      name: ["", [Validators.required, Validators.minLength(6), Validators.email]],
+    });
+
+    this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+  }
+
+  changeColor() {
+    this.color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));;
+  }
 
 }
