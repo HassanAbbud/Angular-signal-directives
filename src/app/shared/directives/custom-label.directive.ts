9651d6188dcf2cc58dc91acac87217a22623b8f9
Hassan Abbud <hassanabbud@hotmail.com>
2024-07-18 18:07:39 -0600

    Directive to show error message depending on error type

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,12 +7,17 @@ import { Directive, ElementRef, Input } from '@angular/core';
 export class CustomLabelDirective {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = "red";
+  private _errors?: ValidationErrors | null;
 
   @Input() set color( value: string ) {
     this._color = value;
     this.setStyle();
   }
 
+  @Input() set errors ( value: ValidationErrors | undefined | null){
+    this._errors = value;
+    this.setErrorMessage();
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el;
@@ -21,6 +27,26 @@ export class CustomLabelDirective {
 
   setStyle(){
     if(!this.htmlElement) return;
+
     this.htmlElement!.nativeElement.style.color = this._color;
   }
+
+  setErrorMessage(){
+    if ( !this.htmlElement )return;
+
+    if ( !this._errors ) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+      }
+
+    const errors = Object.keys(this._errors);
+    console.log(errors)
+
+      if ( errors.includes('required') )  {
+        this.htmlElement.nativeElement.innerText = 'Este campo es requerido.';
+        return;
+      }
+    }
+    //TODO manage other errors
+
 }
